00ab63ed50ddde1792e42adb86d641c12253517f
MuhammadHaseebZIa <124709880+MuhammadHaseebZIa@users.noreply.github.com>
2023-03-25 22:37:10 +0500

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,26 @@
 
-fun main(){
 
-    addStudent()
-    addStudent()
-    searchStudent()
-    displayData()
-    deleteStudent()
-    displayData()
-    insertData()
-    sortData()
+fun main() {
+    while (true) {
+        println("Menu:")
+        println("1. Add Student")
+        println("2. Delete Student")
+        println("3. Search")
+        println("4. Sort")
+        println("5. Display Data")
+        println("6. Insert Data")
+        print("Enter your choice: ")
+        val choice = readLine()?.toIntOrNull() ?: continue
+        
+        when (choice) {
+            1 -> addStudent()
+            2 -> deleteStudent()
+            3 -> searchStudent()
+            4 -> sortStudents()
+            5 -> displayData()
+            6 -> insertData()
+            else -> println("Invalid choice.")
+        }
+    }
+}
 }
